324b21305bb40c4cb5848d729123ce0ba5783f93
Coolightman <drugak48@gmail.com>
2021-11-17 22:47:31 +0300

    add fragment_welcome

--- app/src/main/java/com/coolightman/composition/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/coolightman/composition/presentation/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.coolightman.composition.presentation
+package com.coolightman.composition.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
