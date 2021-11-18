ea417960b9f7618dd6b3ea046dcaedc8019e520e
Coolightman <drugak48@gmail.com>
2021-11-18 21:08:21 +0300

    add viewBinding

--- app/src/main/java/com/coolightman/composition/presentation/fragment/ChooseLevelFragment.kt
+++ app/src/main/java/com/coolightman/composition/presentation/fragment/ChooseLevelFragment.kt
@@ -5,16 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.coolightman.composition.R
+import com.coolightman.composition.databinding.FragmentChooseLevelBinding
 
 
 class ChooseLevelFragment : Fragment() {
 
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance(): ChooseLevelFragment {
+            return ChooseLevelFragment()
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
